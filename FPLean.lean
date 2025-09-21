6ae896714368491399098b1a9b299ec6957b804a
Lean 4 VS Code Extension <>
2025-09-21 11:51:00 +0100

    Initial commit

---
+++ FPLean.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `FPLean` library.
+-- Import modules here that should be built as part of the library.
+import FPLean.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import FPLean
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
